04481fcb47253d7121a50919eb1b528a492f9165
Aurlien-V <153271619+Aurlien-V@users.noreply.github.com>
2024-05-04 02:16:07 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const controllerMusic = require('./../controllers/musics')
-//const {random} = require('./..:controllers/musics')
+const controllerMusic = require('../controller/Music');
 
-router.get('/', (req, res) => {
-    res.status(200).json({ success: 'RACINE API' });
+router.get("/", (req, res) => {
+    res.status(200).json({ success: "racine API" });
 });
 
 router.post('/', (req, res) => {
-    res.status(200).json({ success: 'bounty' });
+    res.status(200).json({ success: 'Bravo' });
 });
 
-router.get('/musics', controllerMusic.find);
 
-router.post('/musics', controllerMusic.create);
-
-router.delete('/musics/:id', controllerMusic.delete);
-
-router.get('/musics/:id', controllerMusic.findById);
-
-router.get('/musics/random', controllerMusic.random);
+router.get('/music', controllerMusic.find);
+router.post('/music', controllerMusic.create);
+router.get('/music/random', controllerMusic.random);
+router.get('/music/:id', controllerMusic.findById);
 
 module.exports = router;
